a78c18e7cf52768a01fbd3544fa3068f4ab79ee9
Epitap8 <shunj783@gmail.com>
2022-09-23 08:18:49 +0900

    2022/9/23 programing first generator and ore (on the way 6) (Complete generator's template)

--- src/main/java/com/epitap/setup/ClientSetup.java
+++ src/main/java/com/epitap/setup/ClientSetup.java
@@ -1,11 +1,15 @@
 package com.epitap.setup;
 
+import com.epitap.blocks.ThermalPowerGeneratorScreen;
 import com.epitap.degeneracycraft.DegeneracyCraft;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ClientSetup {
     public static void setup(final FMLClientSetupEvent event){
-
+        event.enqueueWork(()->{
+            MenuScreens.register(Registration.THERMAL_POWER_GENERATOR_CONTAINER.get(), ThermalPowerGeneratorScreen::new);
+        });
     }
 }
--- src/main/java/com/epitap/setup/Registration.java
+++ src/main/java/com/epitap/setup/Registration.java
@@ -2,12 +2,19 @@ package com.epitap.setup;
 
 import com.epitap.blocks.GravitationOre;
 import com.epitap.blocks.ThermalPowerGenerator;
+import com.epitap.blocks.ThermalPowerGeneratorBE;
+import com.epitap.blocks.ThermalPowerGeneratorContainer;
 import com.epitap.items.Astema;
 import com.epitap.items.GravityIngot;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.common.extensions.IForgeContainerType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fmllegacy.RegistryObject;
@@ -18,11 +25,15 @@ import static com.epitap.degeneracycraft.DegeneracyCraft.MODID;
 public class Registration {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,MODID);
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,MODID);
+    private static final DeferredRegister<BlockEntityType<?>> BLOCKENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES,MODID);
+    private static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS,MODID);
 
     public static void init(){
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ITEMS.register(bus);
         BLOCKS.register(bus);
+        BLOCKENTITIES.register(bus);
+        CONTAINERS.register(bus);
     }
 
 
@@ -41,4 +52,15 @@ public class Registration {
 
     public static final RegistryObject<Item> THERMAL_POWER_GENERATOR_ITEM = ITEMS.register("thermal_power_generator",
             ()-> new BlockItem(THERMAL_POWER_GENERATOR.get(),new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<BlockEntityType<ThermalPowerGeneratorBE>> THERMAL_POWER_GENERATOR_BE = BLOCKENTITIES.register(
+            "thermal_power_generator",()-> BlockEntityType.Builder.of(ThermalPowerGeneratorBE::new,
+                    THERMAL_POWER_GENERATOR.get()).build(null));
+    public static final RegistryObject<MenuType<ThermalPowerGeneratorContainer>> THERMAL_POWER_GENERATOR_CONTAINER =CONTAINERS.register(
+            "thermal_power_generator",()-> IForgeContainerType.create(((windowId, inv, data) -> {
+                BlockPos pos = data.readBlockPos();
+                Level world = inv.player.getCommandSenderWorld();
+                return new ThermalPowerGeneratorContainer(windowId,world,pos,inv,inv.player);
+            }))
+    );
+
 }
